0e0b38ef94e11212c1a82b79cb0b9b81dad94ad2
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-01 23:23:32 +0000

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+. ~/.dod
+
+package install telegram-desktop keepassx zim virt-manager terminator git geany-plugin-treebrowser
+package install webext-https-everywhere webext-privacy-badger webext-keepassxc-browser webext-ublock-origin-firefox
