024371cb8f80ba6cb69de54ac192e6821c7e27b2
debianordie <110401470+debianordie@users.noreply.github.com>
2022-08-02 01:15:52 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . ~/.dod
 
-ensure_dod_module 'system'
+run_dod_module 'system'
 
 package install telegram-desktop keepassx zim virt-manager terminator git geany-plugin-treebrowser
 package install webext-https-everywhere webext-privacy-badger webext-keepassxc-browser webext-ublock-origin-firefox
